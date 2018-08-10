d17fd72b2ba5e35a4fee5a91aedf1d95517d6211
soulfere <soulfere1314@gmail.com>
2018-08-10 23:25:36 +0800

    Add basic update and insert function for orderListDetail.

--- BackendApi/Controllers/OrderListDetailFormController.cs
+++ BackendApi/Controllers/OrderListDetailFormController.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using BackendApi.DB;
+using BackendApi.DB.DataModel;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace BackendApi.Controllers
 {
@@ -34,6 +36,44 @@ namespace BackendApi.Controllers
             else
             {
                 return JsonConvert.SerializeObject(orderListDetailEntity);
+            }
+        }
+
+        // POST api/orderListDetailForm
+        [HttpPost]
+        [EnableCors("CorsPolicy")]
+        // JObject here is very very important...
+        public ActionResult<string> Post([FromBody]JObject entityObj)
+        {
+
+            ORDER_LIST_MST postedEntity = entityObj.ToObject<ORDER_LIST_MST>();
+
+            var orderListEntity = myContext.ORDER_LIST_MST.Where(d => d.ORDER_NO != null);
+
+            if (!String.IsNullOrEmpty(postedEntity.ORDER_NO))
+            {
+                orderListEntity = orderListEntity.Where(d => d.ORDER_NO.Contains(postedEntity.ORDER_NO));
+            }
+
+            if (orderListEntity.Count() == 0)
+            {
+                // INSERT
+                myContext.ORDER_LIST_MST.Add(postedEntity);
+                myContext.SaveChanges();
+                return "";
+            }
+            else
+            {
+                orderListEntity.First().CONTRACT_NO = postedEntity.CONTRACT_NO;
+                orderListEntity.First().PROJECT_NM = postedEntity.PROJECT_NM;
+                orderListEntity.First().ORDER_UNIT = postedEntity.ORDER_UNIT;
+                orderListEntity.First().SALES_PERSON = postedEntity.SALES_PERSON;
+                orderListEntity.First().DEPARTURE_DATE = postedEntity.DEPARTURE_DATE;
+                orderListEntity.First().DELIVERY_DATE = postedEntity.DELIVERY_DATE;
+                orderListEntity.First().REMARK = postedEntity.REMARK;
+                orderListEntity.First().APPLICATION_ENGINEER = postedEntity.APPLICATION_ENGINEER;
+                myContext.SaveChanges();
+                return "";
             }
         }
     }
